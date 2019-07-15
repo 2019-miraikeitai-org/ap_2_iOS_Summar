54f4c444a52cfa698e9034d495990686cc0906e1
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2019-07-15 14:08:48 +0900

    あ

---
+++ iOS-Practice-Team-Working/New Group/SampleModel.swift
@@ -0,0 +1,12 @@
+//
+//  SampleModel.swift
+//  iOS-Practice-Team-Working
+//
+//  Created by reiji kobayashi on 2019/07/15.
+//  Copyright © 2019 reud. All rights reserved.
+//
+
+import Foundation
+
+//  こんな感じでclassを書いていこう
+// 新規ファイルはSwift fileを作成でできるよ！
