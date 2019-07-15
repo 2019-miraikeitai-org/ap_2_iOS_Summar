bc2f230b87fe1af9418148fc6f00f0dd5c0b4d34
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2019-07-15 14:00:00 +0900

    Initial Commnit

---
+++ iOS-Practice-Team-Working/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Practice-Team-Working
+//
+//  Created by reiji kobayashi on 2019/06/27.
+//  Copyright © 2019 reud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
