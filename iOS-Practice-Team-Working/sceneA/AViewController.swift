f0d3d478c41df1411d7ae641de0cb6f6a327b846
rei-yoshi <rei.yosida.5a@stu.hosei.ac.jp>
2019-08-06 13:36:28 +0900

    画面A変更点

--- iOS-Practice-Team-Working/sceneA/AViewController.swift
+++ iOS-Practice-Team-Working/sceneA/AViewController.swift
@@ -5,26 +5,61 @@
 //  Created by reiji kobayashi on 2019/06/27.
 //  Copyright © 2019 reud. All rights reserved.
 //
+//  ViewController.swift
+//  location_0731
+//
+//  Created by 吉田れい on 2019/07/31.
+//  Copyright © 2019 吉田れい. All rights reserved.
+//
 
 import UIKit
+import CoreLocation
 
-class AViewController: UIViewController {
 
+class AViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager!
+    var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        // 緯度、経度表示用のラベル
+        label = UILabel()
+        label.frame.size = CGSize(width: 200, height: 120)
+        label.center = view.center
+        label.numberOfLines = 0
+        view.addSubview(label)
+        
+        locationManager = CLLocationManager()
+        locationManager.requestWhenInUseAuthorization()
+        let status = CLLocationManager.authorizationStatus()
+        switch status {
+        case .restricted, .denied:
+            print("拒否されている")
+            break
+        case .notDetermined:
+            locationManager.delegate = self
+            locationManager.distanceFilter = 10
+            locationManager.startUpdatingLocation()
+        case .authorizedWhenInUse, .authorizedAlways:
+            locationManager.delegate = self
+            locationManager.distanceFilter = 10
+            locationManager?.startUpdatingLocation()
+        default:
+            break
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // 緯度経度に変更があったら呼ばれる
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first
+        let latitude = location?.coordinate.latitude
+        let longitude = location?.coordinate.longitude
+        
+        print("latitude: \(latitude!)\nlongitude: \(longitude!)")
+        label.text = "latitude: \(latitude!)\nlongitude: \(longitude!)"
     }
-    */
-
 }
+
